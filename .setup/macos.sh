b17028ad0d1945702a9a384d8b1fbe3a7663441d
Matt Rathbun <matt@rathbun.cc>
2017-03-22 23:28:55 -0400

    Add some bash scripts to install cool stuff.

---
+++ .setup/macos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+installMaybe () {
+    if ! [ -x "$(command -v $1)" ]; then
+        echo "Installing $1"
+        eval $2
+    else
+        echo "$1 is already installed"
+    fi
+}
+
+# Install homebrew if it isn't installed
+installMaybe brew "/usr/bin/ruby -e $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install some stuff I like
+installMaybe zsh "brew install zsh"
+installMaybe nvim "brew install neovim"
+installMaybe node "brew install node"
+installMaybe yarn "brew install yarn"
+installMaybe ag "brew install the_silver_searcher"
+installMaybe ansible "brew install ansible"
+installMaybe fuck "brew install thefuck"
+
+# Quick look pluginsssss (https://github.com/sindresorhus/quick-look-plugins)
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+brew cask install provisionql quicklookapk
---
+++ .setup/npm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+yarn global add eslint
+yarn global add git+ssh://git@gitlab.com:mathrath/oops.git
+yarn global add emoj
+yarn global add git-standup
