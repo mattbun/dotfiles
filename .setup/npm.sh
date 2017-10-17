d362988b9cd0f1107564449bffc8f893487d74c7
Matt Rathbun <matt@rathbun.cc>
2017-10-16 21:42:01 -0400

    Add eslint to npm setup script.

--- .setup/npm.sh
+++ .setup/npm.sh
@@ -5,6 +5,10 @@ npm install -g git+https://mathrath@gitlab.com/mathrath/oops.git
 npm install -g emoj
 npm install -g git-standup
 
+npm install -g eslint
+npm install -g eslint-config-airbnb-base
+npm install -g eslint-plugin-import
+
 # yarn doesn't want to work for some reason
 #yarn global add eslint
 #yarn global add git+ssh://git@gitlab.com:mathrath/oops.git
