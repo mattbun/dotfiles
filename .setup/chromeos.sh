e7601e18e05b6da7c32c7dda10ce0610b6a730d4
Matt Rathbun <matt@rathbun.cc>
2018-08-28 03:14:43 +0000

    Add a setup script for Chrome OS.

---
+++ .setup/chromeos.sh
@@ -0,0 +1,15 @@
+# You'll need to generate an ssh key in order to clone the dotfiles:
+#   ssh-keygen -t rsa -C "matt@rathbun.cc" -b 4096
+
+# pacman --noconfirm -Sy zsh neovim curl git nodejs npm ripgrep thefuck docker docker-compose
+
+# TODO ripgrep
+# TODO docker (ugh docker in debian is something else)
+# TODO get a newer nodejs
+
+apt-get install -y neovim python-pip python3-pip zsh curl git thefuck docker-compose
+
+# python support for neovim
+pip2 install --user neovim
+pip3 install --user neovim
+
