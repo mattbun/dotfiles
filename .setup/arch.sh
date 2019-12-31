437a0129f0879f6d2f73063c0d9cec0cb24bc754
Matt Rathbun <matt@rathbun.cc>
2019-12-30 22:45:03 -0500

    Update arch setup script

--- .setup/arch.sh
+++ .setup/arch.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
 
-pacman --noconfirm -Sy zsh neovim curl git nodejs npm ripgrep thefuck docker docker-compose
+# TODO install yay (or some other aur tool)
 
-# For graphical install this font
-# pacman -S ttf-hack
+source $(dirname "$0")/common.sh
+
+installPacman () {
+  if [ $# -eq 1 ]; then
+    installMaybe $1 "sudo pacman --noconfirm -S $1"
+  else
+    installMaybe $1 "sudo pacman --noconfirm -S $2"
+  fi
+}
+
+# Some prerequisites to install other things
+installPacman curl
+installPacman git
+installPacman pip python-pip
+
+# zsh
+installPacman zsh
+
+# neovim with python support
+installPacman nvim neovim
+pip3 install --user neovim
+
+# some extras
+installPacman fzf
+installPacman rg ripgrep
+installPacman bat
+installPacman diff-so-fancy
+installPacman htop
+installPacman mc
+installPacman thefuck
+
+# Docker
+installPacman docker
+installPacman docker-compose
+
+# Node.js
+installPacman node nodejs
+installPacman npm
+
+# TODO How to separate out graphical stuff?
+sudo pacman -S --noconfirm ttf-hack
+
+# set default shell to zsh
+sudo chsh -s /usr/bin/zsh $USER
+
+echo "All done!"
