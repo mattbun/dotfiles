46c97753a5722ec19654ebe84165eaffe4216c9a
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2020-11-22 18:06:34 -0500

    Fix gpsup alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,4 +9,4 @@ alias la="exa --git --long -a"
 
 alias gs="git s"
 alias gdca='git diff --cached'
-alias gpsup='git push --set-upstream origin (git_current_branch)'
+alias gpsup='git push --set-upstream origin (git branch --show-current)'
