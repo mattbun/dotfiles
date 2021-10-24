23ae67d1c96cfc46b57c6e8cf6f633bf29f43cd4
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2021-10-24 19:32:51 -0400

    Move git-related mappings to <leader>g

--- .config/nvim/lua/shortcuts.lua
+++ .config/nvim/lua/shortcuts.lua
@@ -10,14 +10,46 @@ map("n", "<leader>]", "<C-i>")
 map("n", "<leader>#", "<Cmd>Telescope grep_string disable_devicons=true<CR>")
 map("n", "<leader>*", "<Cmd>Telescope grep_string disable_devicons=true<CR>")
 map("n", "<leader>~", "<Cmd>Telescope find_files cwd=~ disable_devicons=true<CR>")
-map("n", "<leader>a", "<Cmd>Gitsigns stage_hunk<CR>")
 map("n", "<leader>b", "<Cmd>Telescope buffers disable_devicons=true<CR>")
-map("n", "<leader>B", "<Cmd>Gitsigns toggle_current_line_blame<CR>")
-map("n", "<leader>d", "<Cmd>Gitsigns preview_hunk<CR>")
 map("n", "<leader>fb", "<Cmd>Telescope buffers disable_devicons=true<CR>")
 map("n", "<leader>ff", "<Cmd>Telescope find_files disable_devicons=true<CR>")
 map("n", "<leader>fg", "<Cmd>Telescope live_grep disable_devicons=true<CR>")
-map("n", "<leader>g", "<Cmd>Telescope grep_string disable_devicons=true<CR>")
+map("n", "<leader>ga", "<Cmd>Gitsigns stage_hunk<CR>")
+map("n", "<leader>gb", "<Cmd>Gitsigns toggle_current_line_blame<CR>")
+map("n", "<leader>gd", "<Cmd>Gitsigns preview_hunk<CR>")
+map(
+  "n",
+  "go",
+  '<Cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<CR>'
+)
+map(
+  "v",
+  "go",
+  '<Cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<CR>',
+  {}
+)
+map(
+  "n",
+  "gO",
+  '<Cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<CR>'
+)
+map("n", "<leader>gu", "<Cmd>Gitsigns reset_hunk<CR>")
+map(
+  "n",
+  "<leader>gy",
+  '<Cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".copy_to_clipboard})<CR>'
+)
+map(
+  "v",
+  "<leader>gy",
+  '<Cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".copy_to_clipboard})<CR>',
+  {}
+)
+map(
+  "n",
+  "<leader>gY",
+  '<Cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".copy_to_clipboard})<CR>'
+)
 map("n", "<leader>h", "<Cmd>Telescope oldfiles disable_devicons=true<CR>")
 map("n", "<leader>p", "<Cmd>lua vim.lsp.buf.formatting()<CR>")
 map(
@@ -26,7 +58,6 @@ map(
   "<Cmd>lua vim.g.autoformat = not vim.g.autoformat; if vim.g.autoformat then print 'autoformat enabled' else print 'autoformat disabled' end<CR>"
 )
 map("n", "<leader>t", "<Cmd>NvimTreeToggle<CR>")
-map("n", "<leader>u", "<Cmd>Gitsigns reset_hunk<CR>")
 map("n", "<leader>w", "<Cmd>Telescope lsp_document_diagnostics<CR>")
 map("n", "<leader>W", "<Cmd>Telescope lsp_workspace_diagnostics<CR>")
 map("n", "<leader>y", "<Cmd>let @*=expand('%f')<CR>")
@@ -34,22 +65,6 @@ map("n", "<leader>y", "<Cmd>let @*=expand('%f')<CR>")
 -- Shortcuts that start with `g` are generally for lsp-related things
 map("n", "ga", "<Cmd>Telescope lsp_code_actions<CR>")
 map("v", "ga", "<Cmd>lua vim.lsp.buf.range_code_action()<CR>", {}) -- TODO switch to telescope
-map(
-  "n",
-  "gb",
-  '<Cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<CR>'
-)
-map(
-  "v",
-  "gb",
-  '<Cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<CR>',
-  {}
-)
-map(
-  "n",
-  "gB",
-  '<Cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<CR>'
-)
 map("n", "gd", "<Cmd>Telescope lsp_definitions<CR>")
 map("n", "gh", "<Cmd>lua vim.lsp.buf.hover()<CR>")
 map("n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>")
@@ -58,22 +73,6 @@ map("n", "gr", "<Cmd>Telescope lsp_references<CR>")
 map("n", "gs", "<Cmd>lua vim.lsp.buf.signature_help()<CR>")
 map("n", "gt", "<Cmd>lua vim.lsp.buf.type()<CR>")
 map("n", "gw", "<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false,show_header=false})<CR>")
-map(
-  "n",
-  "gy",
-  '<Cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".copy_to_clipboard})<CR>'
-)
-map(
-  "v",
-  "gy",
-  '<Cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".copy_to_clipboard})<CR>',
-  {}
-)
-map(
-  "n",
-  "gY",
-  '<Cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".copy_to_clipboard})<CR>'
-)
 
 -- Make arrows move, select, and cancel in wildmenu (like when tab completing a command like ':e')
 map("c", "<up>", 'pumvisible() ? "<C-p>" : "<up>"', { noremap = true, expr = true })
