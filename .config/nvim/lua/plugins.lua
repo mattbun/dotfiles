bd5b8ae70baf90d1affd70cc63c82e9be763730f
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2021-10-24 18:28:38 -0400

    Show relative path in lualine

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -196,7 +196,7 @@ return require("packer").startup(function(use)
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch" },
-          lualine_c = { "filename" },
+          lualine_c = { { "filename", path = 1 } },
           lualine_x = { "filetype" },
           lualine_y = { "progress" },
           lualine_z = { "location" },
@@ -204,7 +204,7 @@ return require("packer").startup(function(use)
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = { "filename" },
+          lualine_c = { { "filename", path = 1 } },
           lualine_x = { "location" },
           lualine_y = {},
           lualine_z = {},
