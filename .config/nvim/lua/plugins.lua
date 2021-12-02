a1d8530ddf6c64e4a65f615a0ed0918d992b6fb8
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2021-12-02 15:59:30 -0500

    Make autocomplete not select on enter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -125,7 +125,7 @@ return require("packer").startup(function(use)
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.close(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
         },
         sources = {
           { name = "nvim_lsp" },
