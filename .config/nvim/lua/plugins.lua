59371d4569d537464ea6bf25ec133138d3d1371e
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2022-12-22 12:49:14 -0500

    Replace deprecated 'update_capabilities' with 'default_capabilities'

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -64,7 +64,7 @@ return require("packer").startup(function(use)
       lsp_installer.on_server_ready(function(server)
         -- All lsps get this option
         local opts = {
-          capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+          capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
         }
 
         -- Settings specific to certain lsps
