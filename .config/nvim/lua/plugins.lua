8e7077d8231164f855714b8b30d1199d4ae2e694
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2022-12-27 20:44:04 -0500

    Update configuration disabling tsserver formatting

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -72,8 +72,8 @@ return require("packer").startup(function(use)
         if server.name == "tsserver" then
           -- disable tsserver's formatting in favor of prettier
           opts.on_attach = function(client)
-            client.resolved_capabilities.document_formatting = false
-            client.resolved_capabilities.document_range_formatting = false
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
           end
         end
 
