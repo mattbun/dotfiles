cb7e5057b7e58e6e5bb11fbc8fb0c7a73668b126
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2023-04-27 22:08:48 -0400

    Add 'nfx' script (same as nxx but uses new flake syntax)

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -66,6 +66,12 @@ in
           nix-shell -p $1 --command "$1 ''${@:2}"
         '';
 
+        nfx = ''
+          # Creates a nix shell with the first argument as package and command to run.
+          # Example: `nfx htop`
+          nix shell nixpkgs#$1 --command $1 ''${@:2}
+        '';
+
         nixify = ''
           if [ ! -e ./.envrc ]; then
             echo "use nix" > .envrc
