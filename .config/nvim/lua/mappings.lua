6b2a0d650afbc254016c89dc1400d9550603303e
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2023-10-10 19:28:03 -0400

    Use 'vim.keymap.set' and rewrite mappings in lua

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,94 +1,116 @@
-local map = function(mode, hotkey, cmd, opts)
-  opts = opts or { noremap = true, silent = true }
-  return vim.api.nvim_set_keymap(mode, hotkey, cmd, opts)
+-- pumswitch returns popup_key if the popup menu is visible, otherwise returns not_popup_key.
+local function pumswitch(popup_key, not_popup_key)
+  return function()
+    if vim.fn.pumvisible() == 1 then
+      return popup_key
+    else
+      return not_popup_key
+    end
+  end
 end
 
--- Unmap space so it can be used as leader
-vim.keymap.set("", "<Space>", "", { noremap = true, silent = true })
+local mappings = {
+  -- Unmap space so it can be used as leader
+  [""] = {
+    ["<Space>"] = "",
+  },
 
--- Leader-based shortcuts
-map("n", "<leader><leader>", "<Cmd>Telescope find_files disable_devicons=true<cr>")
-map("n", "<leader>[", "<C-o>")
-map("n", "<leader>]", "<C-i>")
-map("n", "<leader>#", "<Cmd>Telescope grep_string disable_devicons=true<CR>")
-map("n", "<leader>*", "<Cmd>Telescope grep_string disable_devicons=true<CR>")
-map("n", "<leader>~", "<Cmd>Telescope find_files cwd=~ disable_devicons=true<CR>")
-map("n", "<leader>/", "<Cmd>Telescope live_grep disable_devicons=true<CR>")
-map("n", "<leader>\\", "<Cmd>Telescope resume<CR>")
-map("n", "<leader>b", "<Cmd>Telescope buffers disable_devicons=true<CR>")
-map("n", "<leader>d", "<Cmd>Gitsigns preview_hunk<CR>")
--- (f)ind
-map("n", "<leader>fa", "<Cmd>Telescope lsp_code_actions<CR>")
-map("v", "<leader>fa", "<Cmd>lua vim.lsp.buf.range_code_action()<CR>", {}) -- TODO switch to telescope
-map("n", "<leader>fb", "<Cmd>Telescope buffers disable_devicons=true<CR>")
-map("n", "<leader>fd", "<Cmd>Telescope lsp_definitions<CR>")
-map("n", "<leader>ff", "<Cmd>Telescope find_files disable_devicons=true<CR>")
-map("n", "<leader>fg", "<Cmd>Telescope live_grep disable_devicons=true<CR>")
-map("n", "<leader>fh", "<Cmd>Telescope oldfiles disable_devicons=true<CR>")
-map("n", "<leader>fi", "<Cmd>Telescope lsp_implementations<CR>")
-map("n", "<leader>fr", "<Cmd>Telescope lsp_references<CR>")
-map("n", "<leader>ft", "<Cmd>Telescope lsp_type_definitions<CR>")
-map("n", "<leader>fT", "<Cmd>Telescope<CR>")
--- (g)it
-map("n", "<leader>ga", "<Cmd>Gitsigns stage_hunk<CR>")
-map("n", "<leader>gb", "<Cmd>Gitsigns toggle_current_line_blame<CR>")
-map("n", "<leader>gd", "<Cmd>Gitsigns preview_hunk<CR>")
-map(
-  "n",
-  "<leader>go",
-  '<Cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<CR>'
-)
-map(
-  "v",
-  "<leader>go",
-  '<Cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<CR>',
-  {}
-)
-map(
-  "n",
-  "<leader>gO",
-  '<Cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<CR>'
-)
-map("n", "<leader>gu", "<Cmd>Gitsigns reset_hunk<CR>")
-map(
-  "n",
-  "<leader>gy",
-  '<Cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".copy_to_clipboard})<CR>'
-)
-map(
-  "v",
-  "<leader>gy",
-  '<Cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".copy_to_clipboard})<CR>',
-  {}
-)
-map(
-  "n",
-  "<leader>gY",
-  '<Cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".copy_to_clipboard})<CR>'
-)
-map("n", "<leader>h", "<Cmd>lua vim.lsp.buf.hover()<CR>")
-map("n", "<leader>i", "<Cmd>IndentBlanklineToggle<CR>")
-map("n", "<leader>m", "<Cmd>Glow<CR>")
-map("n", "<leader>n", "<Cmd>lua vim.lsp.buf.rename()<CR>")
-vim.keymap.set({ "n", "v" }, "<leader>p", vim.lsp.buf.format)
-map(
-  "n",
-  "<leader>P",
-  "<Cmd>lua vim.g.autoformat = not vim.g.autoformat; if vim.g.autoformat then print 'autoformat enabled' else print 'autoformat disabled' end<CR>"
-)
-map("n", "<leader>r", "<Cmd>Telescope resume<CR>")
-map("n", "<leader>s", "<Cmd>lua vim.lsp.buf.signature_help()<CR>")
-map("n", "<leader>t", "<Cmd>NvimTreeToggle<CR>")
-map("n", "<leader>u", "<Cmd>Gitsigns reset_hunk<CR>")
-map("n", "<leader>w", "<Cmd>lua vim.diagnostic.open_float()<CR>")
-map("n", "<leader>W", "<Cmd>Telescope diagnostics<CR>")
-map("n", "<leader>y", "<Cmd>let @*=expand('%f')<CR>")
+  -- (c)ommand mode
+  c = {
+    -- Make arrows move, select, and cancel in wildmenu (like when tab completing a command like ':e')
+    ["<up>"] = { pumswitch("<c-p>", "<up>"), { noremap = true, expr = true } },
+    ["<down>"] = { pumswitch("<c-n>", "<down>"), { noremap = true, expr = true } },
+    ["<left>"] = { pumswitch("<c-e>", "<left>"), { noremap = true, expr = true } },
+    ["<right>"] = { pumswitch("<c-y>", "<right>"), { noremap = true, expr = true } },
+  },
 
--- `g`-based mappings are often already mapped, be careful adding new ones (use `:help gt`)
-map("n", "gd", "<Cmd>Telescope lsp_definitions<CR>")
+  -- (n)ormal mode
+  n = {
+    ["<leader><leader>"] = require("telescope.builtin").find_files,
+    ["<leader>["] = "<C-o>",
+    ["<leader>]"] = "<C-o>",
+    ["<leader>#"] = require("telescope.builtin").grep_string,
+    ["<leader>*"] = require("telescope.builtin").grep_string,
+    ["<leader>~"] = function()
+      require("telescope.builtin").find_files({ cwd = "~" })
+    end,
+    ["<leader>`"] = require("nvim-tree.api").tree.open,
+    ["<leader>/"] = require("telescope.builtin").live_grep,
+    ["<leader>\\"] = require("telescope.builtin").resume,
+    ["<leader>?"] = require("telescope.builtin").help_tags,
+    ["<leader>a"] = require("telescope.builtin").lsp_code_actions,
+    ["<leader>b"] = require("telescope.builtin").buffers,
+    ["<leader>d"] = require("gitsigns").preview_hunk,
+    ["<leader>f"] = vim.lsp.buf.format,
+    ["<leader>F"] = function()
+      vim.g.autoformat = not vim.g.autoformat
+      if vim.g.autoformat then
+        print("enabled format on write")
+      else
+        print("disabled format on write")
+      end
+    end,
+    ["<leader>ga"] = require("gitsigns").stage_hunk,
+    ["<leader>gb"] = require("gitsigns").toggle_current_line_blame,
+    ["<leader>gd"] = require("gitsigns").preview_hunk,
+    ["<leader>go"] = function()
+      require("gitlinker").get_buf_range_url("n", { action_callback = require("gitlinker.actions").open_in_browser })
+    end,
+    ["<leader>gO"] = function()
+      require("gitlinker").get_repo_url({ action_callback = require("gitlinker.actions").open_in_browser })
+    end,
+    ["<leader>gu"] = require("gitsigns").reset_hunk,
+    ["<leader>gy"] = function()
+      require("gitlinker").get_buf_range_url("n", { action_callback = require("gitlinker.actions").copy_to_clipboard })
+    end,
+    ["<leader>gY"] = function()
+      require("gitlinker").get_repo_url({ action_callback = require("gitlinker.actions").copy_to_clipboard })
+    end,
+    ["<leader>h"] = require("telescope.builtin").oldfiles,
+    ["<leader>i"] = require("indent_blankline.commands").toggle,
+    ["<leader>la"] = require("telescope.builtin").lsp_code_actions,
+    ["<leader>ld"] = require("telescope.builtin").lsp_definitions,
+    ["<leader>lf"] = vim.lsp.buf.format,
+    ["<leader>li"] = require("telescope.builtin").lsp_implementations,
+    ["<leader>ln"] = vim.lsp.buf.rename,
+    ["<leader>lr"] = require("telescope.builtin").lsp_references,
+    ["<leader>ls"] = require("telescope.builtin").lsp_document_symbols,
+    ["<leader>lS"] = require("telescope.builtin").lsp_workspace_symbols,
+    ["<leader>lt"] = require("telescope.builtin").lsp_type_definitions,
+    ["<leader>m"] = "<Cmd>Glow<CR>", -- TODO
+    ["<leader>n"] = vim.lsp.buf.rename,
+    ["<leader>q"] = vim.lsp.buf.hover,
+    ["<leader>s"] = vim.lsp.buf.signature_help,
+    ["<leader>w"] = vim.diagnostic.open_float,
+    ["<leader>W"] = require("telescope.builtin").diagnostics,
+    ["<leader>u"] = require("gitsigns").reset_hunk,
+    ["<leader>y"] = function()
+      vim.fn.setreg("*", vim.fn.expand("%f"))
+    end,
 
--- Make arrows move, select, and cancel in wildmenu (like when tab completing a command like ':e')
-map("c", "<up>", 'pumvisible() ? "<C-p>" : "<up>"', { noremap = true, expr = true })
-map("c", "<down>", 'pumvisible() ? "<C-n>" : "<down>"', { noremap = true, expr = true })
-map("c", "<left>", 'pumvisible() ? "<C-e>" : "<left>"', { noremap = true, expr = true })
-map("c", "<right>", 'pumvisible() ? "<C-y>" : "<right>"', { noremap = true, expr = true })
+    -- `g`-based mappings are often already mapped, be careful adding new ones (use `:help gt`)
+    ["gd"] = require("telescope.builtin").lsp_definitions,
+  },
+
+  -- (v)isual mode
+  v = {
+    ["<leader>f"] = vim.lsp.buf.format,
+    ["<leader>go"] = function()
+      require("gitlinker").get_buf_range_url("v", { action_callback = require("gitlinker.actions").open_in_browser })
+    end,
+    ["<leader>gy"] = function()
+      require("gitlinker").get_buf_range_url("v", { action_callback = require("gitlinker.actions").copy_to_clipboard })
+    end,
+    ["<leader>lf"] = vim.lsp.buf.format,
+  },
+}
+
+for mode, modeMappings in pairs(mappings) do
+  for mapping, action in pairs(modeMappings) do
+    if type(action) == "table" then
+      vim.keymap.set(mode, mapping, action[1], action[2])
+    else
+      vim.keymap.set(mode, mapping, action, { noremap = true, silent = true })
+    end
+  end
+end
