43a38c433824b68fe756c7ad5b575394c31fcffa
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2024-02-28 19:16:34 -0500

    Add default mono and propo fonts

--- .config/nixpkgs/packages/fonts.nix
+++ .config/nixpkgs/packages/fonts.nix
@@ -16,6 +16,18 @@
         description = "The name of the font to use as the default";
         default = "Hack Nerd Font";
       };
+
+      mono = mkOption {
+        type = types.str;
+        description = "The name of the font to use as the default mono font";
+        default = "${config.packageSets.fonts.default} Mono";
+      };
+
+      propo = mkOption {
+        type = types.str;
+        description = "The name of the font to use as the default propo font";
+        default = "${config.packageSets.fonts.default} Propo";
+      };
     };
   };
 
