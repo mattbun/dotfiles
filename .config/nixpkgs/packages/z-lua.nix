1c2e436a655d0d233213642222cf71f7f1bf249f
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2024-11-06 15:00:41 -0500

    Enable enhanced matching in z-lua

--- .config/nixpkgs/packages/z-lua.nix
+++ .config/nixpkgs/packages/z-lua.nix
@@ -5,5 +5,6 @@
 lib.mkIf config.programs.z-lua.enable {
   home.sessionVariables = {
     _ZL_HYPHEN = 1; # Tell z.lua to treat hyphens like normal characters and not part of a regex
+    _ZL_MATCH_MODE = 1; # Use enhanced matching
   };
 }
