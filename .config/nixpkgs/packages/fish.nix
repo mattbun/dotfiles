e60b12587af838f67b6c1e5629acc5e3a9933fa8
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2024-11-06 14:51:29 -0500

    Add 'git_auto_fetch' fish function

--- .config/nixpkgs/packages/fish.nix
+++ .config/nixpkgs/packages/fish.nix
@@ -12,10 +12,23 @@ lib.mkIf config.programs.fish.enable {
     fish = {
       interactiveShellInit = ''
         sh ${nix-colors-lib.shellThemeFromScheme { scheme = config.colorScheme; }}
+
+        # This function needs to be run first for the onVariable to work
+        git_auto_fetch
       '';
 
       functions = {
         fish_greeting = "";
+
+        # Run `git fetch` in the background when entering git repository directories.
+        git_auto_fetch = {
+          onVariable = "PWD";
+          body = ''
+            if test -d ./.git
+              git fetch &> /dev/null &
+            end
+          '';
+        };
       };
     };
 
