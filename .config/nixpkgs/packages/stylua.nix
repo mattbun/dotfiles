1ea3e51dd355426693f0722df3ec140e88dc47cb
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2024-11-07 22:25:55 -0500

    Install stylua

--- .config/nixpkgs/packages/stylua.nix
+++ .config/nixpkgs/packages/stylua.nix
@@ -1,10 +1,15 @@
 { config
 , lib
+, pkgs
 , ...
 }: {
   options.programs.stylua.enable = lib.mkEnableOption "stylua";
 
   config = lib.mkIf config.programs.stylua.enable {
+    home.packages = with pkgs; [
+      stylua
+    ];
+
     xdg.configFile."stylua/stylua.toml" = {
       text = ''
         indent_type = "Spaces"
