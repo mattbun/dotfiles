8f895e6cfd42ede279bc680071603e9aff40bbfc
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2025-10-25 15:40:15 -0400

    Add a default timeout to mako notifications

--- .config/nixpkgs/packages/mako.nix
+++ .config/nixpkgs/packages/mako.nix
@@ -6,5 +6,7 @@
     background-color = "#${config.colorScheme.palette.base00}FF";
     border-color = "#${config.colorScheme.accentColor}FF";
     progress-color = "#${config.colorScheme.palette.base01}FF"; # notify-send -t 2500 "25%" -h "int:value:25"
+
+    default-timeout = 5000;
   };
 }
