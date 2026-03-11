fe4355e33f5b4833b8cc497fff49a163e47b2ea4
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2026-03-11 17:04:51 -0400

    Switch alacritty config to use palette ansi colors

--- .config/nixpkgs/packages/alacritty.nix
+++ .config/nixpkgs/packages/alacritty.nix
@@ -2,7 +2,11 @@
 , config
 , lib
 , ...
-}: {
+}:
+let
+  colors = config.colorScheme.palette;
+in
+{
   fonts.fontconfig.enable = lib.mkIf config.programs.alacritty.enable true;
 
   programs.alacritty = {
@@ -35,38 +39,38 @@
       colors = {
         # Default color;
         primary = {
-          background = "0x${config.colorScheme.palette.base00}";
-          foreground = "0x${config.colorScheme.palette.base05}";
+          background = "0x${colors.base00}";
+          foreground = "0x${colors.base05}";
         };
 
         # Colors the cursor will use if `custom_cursor_colors` is tru";
         cursor = {
-          text = "0x${config.colorScheme.palette.base00}";
-          cursor = "0x${config.colorScheme.palette.base05}";
+          text = "0x${colors.base00}";
+          cursor = "0x${colors.base05}";
         };
 
         # Normal color";
         normal = {
-          black = "0x${config.colorScheme.palette.base00}";
-          red = "0x${config.colorScheme.palette.base08}";
-          green = "0x${config.colorScheme.palette.base0B}";
-          yellow = "0x${config.colorScheme.palette.base0A}";
-          blue = "0x${config.colorScheme.palette.base0D}";
-          magenta = "0x${config.colorScheme.palette.base0E}";
-          cyan = "0x${config.colorScheme.palette.base0C}";
-          white = "0x${config.colorScheme.palette.base05}";
+          black = "0x${colors.ansi00}";
+          red = "0x${colors.ansi01}";
+          green = "0x${colors.ansi02}";
+          yellow = "0x${colors.ansi03}";
+          blue = "0x${colors.ansi04}";
+          magenta = "0x${colors.ansi05}";
+          cyan = "0x${colors.ansi06}";
+          white = "0x${colors.ansi07}";
         };
 
         # Bright color";
         bright = {
-          black = "0x${config.colorScheme.palette.base03}";
-          red = "0x${config.colorScheme.palette.base09}";
-          green = "0x${config.colorScheme.palette.base01}";
-          yellow = "0x${config.colorScheme.palette.base02}";
-          blue = "0x${config.colorScheme.palette.base04}";
-          magenta = "0x${config.colorScheme.palette.base06}";
-          cyan = "0x${config.colorScheme.palette.base0F}";
-          white = "0x${config.colorScheme.palette.base07}";
+          black = "0x${colors.ansi08}";
+          red = "0x${colors.ansi09}";
+          green = "0x${colors.ansi10}";
+          yellow = "0x${colors.ansi11}";
+          blue = "0x${colors.ansi12}";
+          magenta = "0x${colors.ansi13}";
+          cyan = "0x${colors.ansi14}";
+          white = "0x${colors.ansi15}";
         };
 
         draw_bold_text_with_bright_colors = false;
