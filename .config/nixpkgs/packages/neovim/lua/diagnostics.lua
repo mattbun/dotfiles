782b8ef66327b58c0abe0259e9a9a93797405efa
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2026-06-13 17:49:55 -0400

    Use 'vim.diagnostic.config' to fix 'vim.lsp.with() is deprecated' message

--- .config/nixpkgs/packages/neovim/lua/diagnostics.lua
+++ .config/nixpkgs/packages/neovim/lua/diagnostics.lua
@@ -12,7 +12,7 @@ for type, icon in pairs(signs) do
 end
 
 -- Configure how diagnostics are displayed
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+vim.diagnostic.config({
   virtual_text = true,
   signs = true,
   underline = true,
