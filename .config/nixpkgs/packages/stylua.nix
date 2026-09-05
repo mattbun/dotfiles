6994dafe1a61dd1d9b4d95cacfd94b935c857225
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2026-09-05 17:29:27 -0400

    Switch stylua config to use 'programs.stylua'

--- .config/nixpkgs/packages/stylua.nix
+++ .config/nixpkgs/packages/stylua.nix
@@ -1,20 +1,6 @@
-{ config
-, lib
-, pkgs
-, ...
-}: {
-  options.programs.stylua.enable = lib.mkEnableOption "stylua";
-
-  config = lib.mkIf config.programs.stylua.enable {
-    home.packages = with pkgs; [
-      stylua
-    ];
-
-    xdg.configFile."stylua/stylua.toml" = {
-      text = ''
-        indent_type = "Spaces"
-        indent_width = 2
-      '';
-    };
+{ ... }: {
+  programs.stylua.settings = {
+    indent_type = "Spaces";
+    indent_width = 2;
   };
 }
